740e0c1fd075a9c2d6ffd287db165b748807ed31
KAMALESH0081 <kamal20040725@gmail.com>
2025-08-12 17:58:48 +0530

    tokens merger description added within the function

--- utils/tokenizer_utils.py
+++ utils/tokenizer_utils.py
@@ -2,3 +2,10 @@ from transformers import BertTokenizer
 
 def get_tokenizer(model_name="bert-base-uncased"):
     return BertTokenizer.from_pretrained(model_name)
+
+def tokens_merger(token_strings, attention_logits, method="mean"):
+    """this merges the attention logits of the tokens into a single value for particular tokens
+    for example, if the token is '##ing', it will merge the attention logits of 'run' and '##ing' into a single value
+    using operations like sum, average, max etc."""
+
+    pass
